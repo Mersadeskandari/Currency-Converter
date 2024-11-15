89259f5cef505041b58dac4ff08437f088904928
Mersad eskandari <mersadeskandariii@gmail.com>
2024-11-15 12:13:34 +0330

    Currency Converter

---
+++ script.js
@@ -0,0 +1,8 @@
+import ThemeManager from './assets/js/ThemeManager.js';
+import CurrencyConverter from './assets/js/CurrencyConverter.js';
+
+const themeManager = new ThemeManager();
+themeManager.init()
+const currencyConverter = new CurrencyConverter();
+
+currencyConverter.init();
